0e3b391f9c76218be5c9cf70475bc12297e3776d
Steve Kondik <shade@chemlab.org>
2014-10-07 23:14:32 -0700

    stagefright: More high-resolution audio support
     * Clean up the "error driven" fallback code we had before and
       reconfigure components if the output bit width needs to change
       (a2dp/usb fallback).
     * Decode various formats (AC3/DTS/EAC3) which output floating point
       samples using FFMPEG, which will output 24-bit PCM for us.
    
    Change-Id: I94f5fa59f5f737be8969df4c5ad4295c45cb8657
    
    Conflicts:
    	media/libstagefright/AwesomePlayer.cpp

--- media/libstagefright/AwesomePlayer.cpp
+++ media/libstagefright/AwesomePlayer.cpp
@@ -1913,6 +1913,7 @@ status_t AwesomePlayer::initAudioDecoder() {
             mAudioSource = mAudioTrack;
 #ifndef QCOM_DIRECTTRACK
         } else {
+            mOmxSource->getFormat()->setInt32(kKeySampleBits, 16);
             mAudioSource = mOmxSource;
 #endif
         }
@@ -1924,7 +1925,7 @@ status_t AwesomePlayer::initAudioDecoder() {
     mAudioTrack->getFormat()->findInt32(kKeySampleBits, &bitsPerSample);
 
     if (!mOffloadAudio && mAudioSource != NULL) {
-        ALOGW("Could not offload audio decode, try pcm offload");
+        ALOGI("Could not offload audio decode, try pcm offload (%d-bit)", bitsPerSample);
         sp<MetaData> format = mAudioSource->getFormat();
         if (durationUs >= 0) {
             format->setInt64(kKeyDuration, durationUs);
