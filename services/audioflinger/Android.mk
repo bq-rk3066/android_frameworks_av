b2233d7110891fddb9dfe8c18079f5091f4b424e
Steve Kondik <shade@chemlab.org>
2014-10-09 10:54:09 -0700

    libmedia: Add flag for non-native FAST tracks.
    Change-Id: Ied9004c0bdc6314541b12095ca1defd0f291eb1a
    
    Conflicts:
    	services/audioflinger/Android.mk

--- services/audioflinger/Android.mk
+++ services/audioflinger/Android.mk
@@ -88,6 +88,10 @@ LOCAL_SRC_FILES += FastMixer.cpp FastMixerState.cpp AudioWatchdog.cpp
 
 LOCAL_CFLAGS += -DSTATE_QUEUE_INSTANTIATIONS='"StateQueueInstantiations.cpp"'
 
+ifneq ($(AUDIO_FEATURE_ENABLED_ULTRA_LOW_LATENCY),true)
+LOCAL_CFLAGS += -DNATIVE_FAST_TRACKS_ONLY
+endif
+
 # Define ANDROID_SMP appropriately. Used to get inline tracing fast-path.
 ifeq ($(TARGET_CPU_SMP),true)
     LOCAL_CFLAGS += -DANDROID_SMP=1
